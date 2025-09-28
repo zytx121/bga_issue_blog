3b023fdc0418aa0db43b5c221111358ff4975c9a
zytx121 <592267829@qq.com>
2025-09-28 12:56:40 +0800

    update

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,19 +1,19 @@
 class Config {
   // 配置个人 GitHub 名称
-  static final gitHubUsername = 'bingoogolapple';
+  static final gitHubUsername = 'zytx121';
 
   // 根据 GitHub 名称自动组装存放 issues 的仓库
   static final repo = '$gitHubUsername/$gitHubUsername.github.io';
 
   // 配置个人链接图片映射
   static final personalLinkMap = {
-    'images/github.png': 'https://github.com/bingoogolapple',
-    'images/git.png': 'https://bingoogolapple.gitbooks.io/bgalearningnotes-git',
+    'images/github.png': 'https://github.com/zytx121',
+    'images/git.png': 'https://github.com/zytx121',
   };
 
   // QQ 配置信息
-  static final qqTitle = 'BGA 系列\n';
-  static final qqDesc = '开源库 QQ 群';
+  static final qqTitle = '大模型饲养猿 \n';
+  static final qqDesc = '微信公众号';
   static final qqQrImg = 'images/qq-group.png';
 
   // 网站备案信息
